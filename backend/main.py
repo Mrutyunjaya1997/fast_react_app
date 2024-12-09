6c069a33a76a341e7a65bfb418d5a3b2344c1cda
Mrutyunjaya Sahoo <Mrutyunjaya.Sahoo@novigo.com>
2024-12-09 23:16:28 +0530

    front end build added in the backend folder

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow React app
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/greet/{name}")
+# Serve React static files
+app.mount("/", StaticFiles(directory="frontend-build", html=True), name="static")
+
+@app.get("/api/greet/{name}")
 async def greet(name: str):
     return {"message": f"Hello, {name}!"}
