e6d482219ef81c9262a4125f9859d625aba2b574
Mrutyunjaya Sahoo <Mrutyunjaya.Sahoo@novigo.com>
2024-12-10 12:20:21 +0530

    frontend web url updated in the main file in backend file

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,22 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:3000"],  # Allow React app
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow React app
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=[
+        "https://react-front-end-m6xa.onrender.com",  # Frontend deployed on Render
+    ],
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+    allow_credentials=True,  # Allow cookies or authorization headers
 )
 
 @app.get("/greet/{name}")
